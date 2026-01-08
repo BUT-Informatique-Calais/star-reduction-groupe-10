e65ebb1a31ab28742586c13a9ccca8e987aeb2b9
quentinBOSSUS <quentplop@gmail.com>
2026-01-08 15:06:13 +0100

    Adding light/dark theme and UI update

--- ImageView.py
+++ ImageView.py
@@ -1,18 +1,20 @@
-﻿from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QSlider, QGridLayout, QApplication
+﻿from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QSlider, QGridLayout
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtGui import QPixmap, QImage, QIcon
 
 class ImageView(QWidget):
     def __init__(self):
         super().__init__()
         
         # Main window configuration
-        self.setWindowTitle("Star Reduction MVC")
+        self.setWindowTitle("Star Reduction")
         self.setGeometry(100, 100, 1200, 800)
 
         # Buttons
         self.load_button = QPushButton("Importer une image")
         self.save_button = QPushButton("Exporter l'image")
+        self.theme_button = QPushButton()
+        self.theme_button.setIcon(QIcon("img/sun.png"))
 
         # Star radius slider
         self.star_radius = QSlider(Qt.Orientation.Horizontal)
@@ -47,14 +49,53 @@ class ImageView(QWidget):
         layout = QGridLayout()
         layout.addWidget(self.load_button, 0, 0)
         layout.addWidget(self.save_button, 0, 1)
+        layout.addWidget(self.theme_button, 0, 2)
         layout.addWidget(self.label_orig, 1, 0)
         layout.addWidget(self.label_result, 1, 1)
-        layout.addWidget(QLabel("Rayon des étoiles"), 2, 0)
+                
+        label_star_radius = QLabel("Rayon des étoiles")
+        label_star_radius.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(label_star_radius, 2, 0)
         layout.addWidget(self.star_radius, 3, 0)
-        layout.addWidget(QLabel("Filtre médian"), 2, 1)
+
+        label_median = QLabel("Filtre médian")
+        label_median.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(label_median, 2, 1)
         layout.addWidget(self.median_filter, 3, 1)
 
+
         self.setLayout(layout)
+        
+        # Mode
+        self.dark_mode = True
+        self.theme_button.clicked.connect(self.toggle_theme)
+        self.set_dark_theme()
+
+    def toggle_theme(self):
+        if self.dark_mode:
+            self.set_light_theme()
+            self.dark_mode = False
+            self.theme_button.setIcon(QIcon("img/moon.png"))
+        else:
+            self.set_dark_theme()
+            self.dark_mode = True
+            self.theme_button.setIcon(QIcon("img/sun.png"))
+            
+
+    def load_stylesheet(self, file_path):
+        """Load QSS file"""
+        with open(file_path, "r") as f:
+            return f.read()
+
+    def set_dark_theme(self):
+        """Set dark theme"""
+        qss = self.load_stylesheet("style/dark_theme.qss")
+        self.setStyleSheet(qss)
+
+    def set_light_theme(self):
+        """Set light theme"""
+        qss = self.load_stylesheet("style/light_theme.qss")
+        self.setStyleSheet(qss)
 
     def update_image(self, label: QLabel, image):
         """Convert numpy image to QPixmap and update label"""
